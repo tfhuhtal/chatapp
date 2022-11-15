3183dc2e4f9cc959e219ff67ad9b1f725ba89e6a
Tuomas Huhtala <tuomas.huhtala@icloud.com>
2022-11-15 13:08:09 +0200

    adding docstring

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from os import getenv
 from flask import Flask
 from flask import redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
@@ -9,8 +10,9 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
+    '''Returns index.html with counter in it'''
     db.session.execute("INSERT INTO visitors (time) VALUES (NOW())")
     db.session.commit()
     result = db.session.execute("SELECT COUNT(*) FROM visitors")
-    counter = 0 #result.fetchone()[0]
+    counter = result.fetchone()[0]
     return render_template("index.html", counter=counter) 
