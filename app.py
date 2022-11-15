9954a7823c90e7527d67382a1ab1f40f8b4a3201
Tuomas Huhtala <tuomas.huhtala@icloud.com>
2022-11-15 12:51:16 +0200

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -12,5 +12,5 @@ def index():
     db.session.execute("INSERT INTO visitors (time) VALUES (NOW())")
     db.session.commit()
     result = db.session.execute("SELECT COUNT(*) FROM visitors")
-    counter = result.fetchone()[0]
+    counter = 0 #result.fetchone()[0]
     return render_template("index.html", counter=counter) 
