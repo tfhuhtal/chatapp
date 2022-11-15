b8e15ec06e7bb80cde83c8036aec0d05b59f266f
Tuomas Huhtala <tuomas.huhtala@icloud.com>
2022-11-15 15:44:48 +0200

    schema update

--- schema.sql
+++ schema.sql
@@ -1,1 +1,29 @@
-CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
+CREATE TABLE visitors (
+    id SERIAL PRIMARY KEY,
+    time TIMESTAMP
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE participants (
+    id SERIAL PRIMARY KEY,
+    userID INTEGER,
+    roomID INTEGER
+);
+
+CREATE TABLE rooms (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    roomID INTEGER,
+    userID INTEGER,
+    message TEXT,
+    sent_at TIMESTAMP
+);
